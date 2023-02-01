dee06dd689b81eb33987a3f28963869085bf72c2
Calaser <s33003030@gmail.com>
2023-02-01 09:44:48 +0800

    A

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          deploy-on-vercel-on-local-file
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://vercel.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learning deploy on vercel
         </a>
       </header>
     </div>
