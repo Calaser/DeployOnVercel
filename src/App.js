7612c119698ac32b45ed8b5a45c0ed94f93504da
Calaser <s33003030@gmail.com>
2023-02-01 09:46:22 +0800

    !

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          deploy-on-vercel-on-hub
+          deploy-on-vercel-on-github
         </p>
         <a
           className="App-link"
